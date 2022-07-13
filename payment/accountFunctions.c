a8d309e7b9acca5a78bf81b39795afc5150d523b
falcao00 <nickbartowskireserva@gmail.com>
2022-07-13 03:08:33 -0300

    [PAY-3-1] Corrigindo Idetancao

--- payment/accountFunctions.c
+++ payment/accountFunctions.c
@@ -12,27 +12,28 @@ typedef struct{
 } data;
 
 int accountCreation(void){
-        data acc;
-        char data[DATA_SIZE];
-        printf("Inserir Nome:");
-        scanf("%s", acc.name);
-         printf("\n");
-          printf("Inserir Senha:");
-         scanf("%s", acc.password);
-         printf("\n");
-         printf("Confirme as Informações: [Nome: %s];[Senha: %s]\n", acc.name, acc.password);
-         sprintf(data, "[Nome: %s];[Senha: %s]", acc.name, acc.password);
+  data acc;
+  char data[DATA_SIZE];
+  
+  printf("Inserir Nome:");
+  scanf("%s", acc.name);
+  printf("\n");
+  printf("Inserir Senha:");
+  scanf("%s", acc.password);
+  printf("\n");
+  printf("Confirme as Informações: [Nome: %s];[Senha: %s]\n", acc.name, acc.password);
+  sprintf(data, "[Nome: %s];[Senha: %s]", acc.name, acc.password);
 
-        FILE * fileAccount;
-        fileAccount = fopen("../accounts/account_id_x.txt", "w");
-        if(fileAccount == NULL){
-          printf("Erro ao Criar Arquivo\n");
-          exit(EXIT_FAILURE);
-        }
-        fgets(data, DATA_SIZE, stdin);
-        fputs(data, fileAccount);
-        fclose(fileAccount);
-        printf("File created and saved successfully. :) \n");
+  FILE * fileAccount;
+  fileAccount = fopen("../accounts/account_id_x.txt", "w");
+  if(fileAccount == NULL){
+    printf("Erro ao Criar Arquivo\n");
+    exit(EXIT_FAILURE);
+  }
+  fgets(data, DATA_SIZE, stdin);
+  fputs(data, fileAccount);
+  fclose(fileAccount);
+  printf("File created and saved successfully. :) \n");
 
   return 0;
 }
