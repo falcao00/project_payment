04031c0a722f177dcb1b281754fbc6186e0666d3
falcao00 <nickbartowskireserva@gmail.com>
2022-07-13 01:32:43 -0300

    [PAY-2] Função de criacao de conta teste

---
+++ payment/accountFunctions.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "accountFunctions.h"
+
+int accountCreation(void){
+    
+        printf("Por favor inserir as seguintes informações: Nome, Senha");
+
+  return 0;
+}
--- payment/main.c
+++ payment/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include "accountFunctions.h"
 
 int main(void){
   _Bool aplicationMain = true;
   while(aplicationMain){
     printf("Bem vindo Usuário, Qual operação deseja executar?\n\n1 - Criar uma conta\n\n2 - Acessar uma conta\n\n3 - Fazer uma Transação\n\n");
-    printf("4 - Sair\n\n");
     int functionValue = 0;
     scanf("%d", &functionValue);
     switch(functionValue){
-    
       case 1:
         printf("Função de Criação de Conta\n");
+        accountCreation();
         break;
       case 2:
         printf("Função de Acessar uma conta existente\n");
@@ -19,11 +19,8 @@ int main(void){
       case 3:
         printf("Função Transacional\n");
         break;
-      case 4:
-        aplicationMain = false;
-        break;
       default:
-        printf("Por favor inserir uma opção valida\n");
+        aplicationMain = false;
         break;
     };
   };
